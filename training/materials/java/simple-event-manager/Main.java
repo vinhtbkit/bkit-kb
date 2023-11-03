46abd1afd4d957840889ac8189a94da2ab8123bc
vinhtbkit <149031014+vinhtbkit@users.noreply.github.com>
2023-11-03 11:23:30 +0700

    Update Main.java

--- training/materials/java/simple-event-manager/Main.java
+++ training/materials/java/simple-event-manager/Main.java
@@ -19,29 +19,5 @@ class Main {
         }
       } 
     }
-    
   }
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-class Main {
-  public static void main(String[] args) throws IOException {
-    String input;
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-      String str = ""; 
-      System.out.println("Enter the Strings and when you want to stop entering the Strings, type ‘exit’");
-      while(!str.equals("exit")) { 
-          System.out.println("Enter a method: "); 
-          str = br.readLine(); 
-
-        // TODO: your logic here
-
-        if(str.contentEquals("exit")) {
-              System.out.println("FullStop!!!");
-        }
-      } 
-    }
-    
-  }
-}}
+}
